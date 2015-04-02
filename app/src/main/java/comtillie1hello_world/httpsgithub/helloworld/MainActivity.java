cb20aa04807d57fd85616bf883c2dcdd7e52494b
Tillie1 <mariannetruijen@gmail.com>
2015-04-02 16:32:49 +0200

    Versie7

--- app/src/main/java/comtillie1hello_world/httpsgithub/helloworld/MainActivity.java
+++ app/src/main/java/comtillie1hello_world/httpsgithub/helloworld/MainActivity.java
@@ -11,32 +11,36 @@ import android.widget.EditText;
 public class MainActivity extends ActionBarActivity {
 
     /*
- * Called when the player clicks the button.
+ * Called when the player clicks one button.
  */
-    public void onBtnClick(View view) {
+    public void onBtnClickGroep(View view) {
         EditText txtBedrag = (EditText) findViewById(R.id.editBedrag);
         EditText txtTotaalBedrag = (EditText) findViewById(R.id.editTotaalBedrag);
         EditText txtPercentage = (EditText) findViewById(R.id.editPercentage);
         float p = Float.parseFloat(txtPercentage.getText().toString());
         float b = Float.parseFloat(txtBedrag.getText().toString());
         float totaal = (float) (b*(1.0+(p/100.0)));
-        txtTotaalBedrag.setText(String.format("%s", totaal));
-        //EditText txtPersonen = (EditText) findViewById(R.id.editPersonen);
-        //EditText txtBedragPerPersoon = (EditText) findViewById(R.id.editTextBedragpp);
-        //float pp = Float.parseFloat(txtPersonen.getText().toString());
-        //float totaalpp = (float) ((b*(1.0+(p/100.0)))/pp);
-        //txtBedragPerPersoon.setText(String.format("%s", totaalpp));
-        //Button left = (Button) findViewById(R.id.ButtonGroep);
+        txtTotaalBedrag.setText(String.format("%.2f", totaal));
     }
 
-
+    public void onBtnClickPersoon(View view) {
+        EditText txtBedrag = (EditText) findViewById(R.id.editBedrag);
+        EditText txtPercentage = (EditText) findViewById(R.id.editPercentage);
+        EditText txtPersonen = (EditText) findViewById(R.id.editPersonen);
+        EditText txtBedragPerPersoon = (EditText) findViewById(R.id.editBedragPerPersoon);
+        float pp = Float.parseFloat(txtPersonen.getText().toString());
+        float p = Float.parseFloat(txtPercentage.getText().toString());
+        float b = Float.parseFloat(txtBedrag.getText().toString());
+        float totaalpp = (float) ((b*(1.0+(p/100.0)))/pp);
+        txtBedragPerPersoon.setText(String.format("%.2f", totaalpp));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.setTitle("Custom Dialog");
+        this.setTitle("Fooi App");
     }
 
 
